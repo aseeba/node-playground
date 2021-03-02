720f5dc128a23c617226ce69d3cf863b4f1111bc
Anja Seeba <ahoi@achtsam.media>
2021-03-02 16:40:17 +0100

    Testing

--- write.js
+++ write.js
@@ -1,15 +1,20 @@
 const fs = require('fs') // files system tools
 const { exit } = require('process') // exit
 
-const file = 'tools.json' // wie heißt die konstante wo wir rein speichern?
-const tools = process.argv[2] // hier tragen wir den process in tools ein
+const file = 'tools.json' // Datei in die wir speichern
+const tools = process.argv[2] 
+const rating = process.argv[3]
 
 fs.readFile(file, { encoding: 'utf-8' }, (err, data) => {
   err && exit(1)
-  const tools = JSON.parse(data)
-  tools.push({ name: tool })
+  const tools = err ? [] : JSON.parse(data)
+  tools.push({ name: tool, rating })
 })
 
 fs.writeFile(file, JSON.stringify(tools), { encoding: 'utf-8' }, err => {
-  exit(err ? 1 : 0)
+  if (!err) {
+      tools.forEach(tool => { 
+          console.log(tool.name + '/t' + tool.rating)
+  }
+    exit(err ? 1 : 0)
 })
